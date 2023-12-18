208d93101653e079e1e7066ac5ce0a565074cda3
Atharva <atharvakumbhar18@gmail.com>
2023-12-18 22:53:21 +0530

    Day_17

---
+++ Happy_Number.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool isHappy(int n) {
+        unordered_set<int> usedIntegers;
+        while(true){
+            int sum=0;
+            while (n!=0){
+                sum +=pow(n%10,2.0);
+                n=n/10;
+            }
+            if (sum==1) return true;
+            n=sum;
+            if(usedIntegers.find(n)!=usedIntegers.end())
+            return false;
+            usedIntegers.insert(n);
+        }
+    }
