8e574470a19dfa667edd932e604d1286de0357bc
Atharva <atharvakumbhar18@gmail.com>
2023-12-19 22:55:32 +0530

    Day_18

---
+++ Largest_Number.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    static bool check(const string &s1, const string &s2) {
+        return (s1 + s2) > (s2 + s1);
+    }
+
+    string largestNumber(vector<int>& nums) {
+        int x = 0;
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[i] == 0) {
+                x++;
+            }
+        }
+        if (x == nums.size()) {
+            return "0";
+        }
+
+        vector<string> s(nums.size(), "");
+
+        for (int i = 0; i < nums.size(); i++) {
+            s[i] = to_string(nums[i]);
+        }
+
+        sort(s.begin(), s.end(), check);
+        string ans = "";
+
+        for (int i = 0; i < s.size(); i++) {
+            ans += s[i];
+        }
+
+        return ans;
+    }
+};
