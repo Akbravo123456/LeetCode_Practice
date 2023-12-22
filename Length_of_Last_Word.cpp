45bcfd94a39dab6239869088a223b20b66159f45
Atharva <atharvakumbhar18@gmail.com>
2023-12-22 21:58:40 +0530

    Day_21

---
+++ Length_of_Last_Word.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+        int count = 0;
+        for (int i = s.length() - 1; i >= 0; i--) {
+            if (s[i] != ' ') { 
+                count++;
+            } else {
+                if (count > 0) {
+                    return count;
+                }
+            }
+        }
+        return count;
+    }
+};
