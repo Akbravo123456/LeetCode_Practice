a54b9e23228e9adddc7515211e186e6ac2ec169a
Atharva <atharvakumbhar18@gmail.com>
2024-02-25 15:32:11 +0530

    Day_86

---
+++ Valid_Sudoku.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool isValidSudoku(vector<vector<char>>& board) {
+        unordered_set<string> seen;
+        for(int i=0;i<9;i++){
+            for(int j=0;j<9;j++){
+                if(board[i][j]!='.'){
+                     if (!seen.insert("row" + to_string(i) + board[i][j]).second ||
+                     !seen.insert("column" + to_string(j) + board[i][j]).second ||
+                     !seen.insert("box" + to_string((i / 3) * 3 + j / 3) + board[i][j]).second){
+                         return false;
+                     }
+                }
+            }
+        }
+        return true;
+    }
+};
