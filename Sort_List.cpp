5cb55d276af0083ef40dd4c5c11d08be7327b300
Atharva <atharvakumbhar18@gmail.com>
2024-03-10 20:21:28 +0530

    Day_100

---
+++ Sort_List.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    ListNode* sortList(ListNode* head) {
+        if(!head||!head->next)
+        return head;
+
+        ListNode* slow=head;
+        ListNode* fast=head->next;
+
+        while(fast && fast->next){
+            slow=slow->next;
+            fast=fast->next->next;
+        }
+
+        ListNode* mid=slow->next;
+        slow->next=NULL;
+        
+        ListNode* left=sortList(head);
+        ListNode* right=sortList(mid);
+        return merge(left,right);
+}
+
+private:
+ListNode* merge(ListNode* l1,ListNode* l2){
+    ListNode dummy(0);
+    ListNode* tail = &dummy;
+
+    while(l1 && l2){
+        if(l1->val < l2->val){
+            tail->next=l1;
+            l1=l1->next;
+        }else{
+            tail->next=l2;
+            l2=l2->next;
+        }
+        tail=tail->next;
+    }
+    tail->next=(l1 ? l1:l2);
+    return dummy.next;
+}
+};
