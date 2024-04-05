03b3f24c94e698ce1bf2c5ec7c58703e47c04bd0
Atharva <atharvakumbhar18@gmail.com>
2024-04-05 23:31:16 +0530

    Day_126

---
+++ Set_Matrix_Zeroes.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    void setZeroes(vector<vector<int>>& matrix) {
+        int n=matrix.size();
+        int m=matrix[0].size();
+        vector<vector<int>> v=matrix;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(matrix[i][j]==0){
+                    for(int k=0;k<m;k++){
+                        v[i][k]=0;
+                    }
+                }
+            }
+        }
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(matrix[i][j]==0){
+                    for(int k=0;k<n;k++){
+                        v[k][j]=0;
+                    }
+                }
+            }
+        }
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                matrix[i][j]=v[i][j];
+            }
+        }
+    }
+};
