b3e1131b411c946d98dfad2dd76f4b28ef59f096
Atharva <atharvakumbhar18@gmail.com>
2024-07-01 22:43:14 +0530

    Day_213

---
+++ Three_Consecutive_Odds.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool threeConsecutiveOdds(vector<int>& arr) {
+        int a=arr.size();
+        for(int i=0;i<=a-3;i++){
+            if(arr[i]%2!=0 && arr[i+1]%2!=0 && arr[i+2]%2!=0){
+                return true;
+            }
+        }
+        return false;
+    }
+};
