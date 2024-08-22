37c32bd7773b4f9d0e5784cf7812cf6d4de50ee2
Atharva <atharvakumbhar18@gmail.com>
2024-08-22 21:46:47 +0530

    Day_262

--- Kth_Largest_Element_in_a_Stream.cpp
+++ Kth_Largest_Element_in_a_Stream.cpp
@@ -21,8 +21,3 @@ int size;
     }    
 };
 
-/**
- * Your KthLargest object will be instantiated and called as such:
- * KthLargest* obj = new KthLargest(k, nums);
- * int param_1 = obj->add(val);
- */
